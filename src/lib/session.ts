fe757c3b4c5a7d5a02228003adf0874ad47de33e
Pattachitra Audio <pattachitra.audio@gmail.com>
2025-10-15 01:02:59 +0530

    fixed build errors

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -8,7 +8,7 @@ const COOKIE_NAME = "jwt";
 
 const COOKIE_OPTIONS = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "prod",
+    secure: process.env.NODE_ENV === "production",
     sameSite: "lax" as const,
     maxAge: 60 * 60 * 24 * 7, // 7 days
     path: "/",
