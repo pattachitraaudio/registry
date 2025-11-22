b98f739abbdfd6436753049a5748fddafade806b
Pattachitra Audio <pattachitra.audio@gmail.com>
2025-11-22 14:52:52 +0530

    Removed unused file, (causing build errors)

--- src/lib/db.ts
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from "mongodb";
-import { Globals } from "@/globals/backend";
-
-const mongoClient = new MongoClient(Globals.env.mongoDB.CONNECTION_URI, { appName: Globals.appInfo.name });
-mongoClient.connect();
-
-export { mongoClient };
