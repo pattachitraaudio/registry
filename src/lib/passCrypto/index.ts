932479416784317683a5ddeb06dabd00d6dc18e7
Pattachitra Audio <pattachitra.audio@gmail.com>
2025-11-23 13:37:28 +0530

    For adding encryption to the passwords

---
+++ src/lib/passCrypto/index.ts
@@ -0,0 +1,15 @@
+import crypto from "crypto";
+
+/*
+const passCrypto = async function () {
+    // await env
+function encrypt(str: string): string {
+    const initializationVector = crypto.randomBytes(12);
+    const cipher = crypto.createCipheriv("aes-256-gcm", )
+}
+
+function decrypt(str: string): string {}
+
+}
+
+*/
